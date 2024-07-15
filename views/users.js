803f9d4ae288b2af3567b7cc55c13e5f2d322a9b
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-07-15 14:38:17 -0400

    added router outlines

---
+++ views/users.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// outline page routes
+router
+    .route("/")
+    .get((req, res) => {
+    })
+    .post((req, res) => {
+    })
+    .patch((req, res) => {
+    })
+    .delete((req, res) => {
+    });
