f91993d63c9203f80b5fb8e3c2cb196e2aed8b59
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-07-17 20:08:09 -0400

    created GET routes for logs and users

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const usersdata = require("../data/users");
 
 // outline page routes
-router
-    .route("/")
-    .get((req, res) => {
-    })
-    .post((req, res) => {
-    })
-    .patch((req, res) => {
-    })
-    .delete((req, res) => {
-    });
+
+
+//get route via search query
+router.get("/", (req, res) => {
+    if (req.query.user) {
+        let result = usersdata.find(user => user.id === Number(req.query.user));
+        res.send(result);
+    }
+    else res.status(404).send("Not found");
+});
+
+//get route via path params
+router.get("/:id", (req, res) => {
+    let result = usersdata.find(user => user.id === Number(req.params.id));
+    result ? res.send(result) : res.status(404).send("Not found");
+});
+
+// // outline page routes
+// router
+//     .route("/")
+//     .get((req, res) => {
+//     })
+//     .post((req, res) => {
+//     })
+//     .patch((req, res) => {
+//     })
+//     .delete((req, res) => {
+//     });
 
 module.exports = router;
